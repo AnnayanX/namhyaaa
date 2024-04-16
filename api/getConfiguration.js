51ff6ab89a496aea4748adc4f5435820bff49510
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-04-16 13:25:18 +0530

    Create getConfiguration.js

---
+++ api/getConfiguration.js
@@ -0,0 +1,17 @@
+// api/getConfiguration.js
+const handler = (req, res) => {
+  try {
+    // Your bot token (hidden on the server side)
+    const botToken = process.env.BOT_TOKEN;
+
+    // Your chat ID (replace this with your actual chat ID)
+    const chatId = '-1001353709350';
+
+    // Provide configuration to the client side
+    return res.status(200).json({ botToken, chatId });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = handler;
